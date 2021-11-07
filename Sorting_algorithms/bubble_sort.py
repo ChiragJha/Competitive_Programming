d355c3c5f69e7ef26b6e42eeaf6278dbbcca6216
ChiragChandraJha <cjdon911@gmail.com>
2021-11-07 17:13:57 +0530

    Added algo for bubble sort
    Signed-off-by: ChiragChandraJha <cjdon911@gmail.com>

---
+++ Sorting_algorithms/bubble_sort.py
@@ -0,0 +1,31 @@
+# Called bubble because it works as if the largest bubbles keep moving to the end based on their size and
+# sort themselves accordingly. Then larger bubbles move to the end and they hold up the smaller ones in a sorted
+# manner
+
+# This algorithm compares adjacent elements and moves the largest element in each pass to the respective position.
+# In the first pass, largest element is moved to the end. In the second pass, 2nd largest element is moved to the
+# 2nd last position and so on. In n-1 passes, the array is sorted.
+
+# Time complexity = O(n2)
+# Space complexity = O(1)
+
+def swap_places(array, index):
+    temp = array[index + 1]
+    array[index + 1] = array[index]
+    array[index] = temp
+
+
+def bubble_sort(array):
+    for index_outer in range(len(array) - 1):
+        # Since after each pass the last element is always sorted(largest), there is no need to compare.
+        # optimising the second loop to neglect the last_index - current_pass
+        # for index_inner in range(len(array)-1):
+        for index_inner in range(len(array) - index_outer - 1):
+            if array[index_inner] > array[index_inner + 1]:
+                swap_places(array, index_inner)
+    return array
+
+
+# Define a test dataSet
+array = [1, 10, 0, -1, 20]
+print(bubble_sort(array))
